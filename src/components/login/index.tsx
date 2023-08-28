377040874da77b317f82ba97cb6b04b38c4f20c9
eyluldnz <eyluldnzcn@gmail.com>
2023-08-29 00:28:27 +0300

    Installed formik, antd and yup packages. Created validationSchema and initalValues

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,10 +1,18 @@
+import { useFormik } from "formik";
+import { getInitalValues, validationSchema } from "./formHelper";
+
 function Login() {
-    return (
-      <div>
-       Login Page
-      </div>
-    );
-  }
-  
-  export default Login;
-  
+  const formik = useFormik({
+    initialValues: getInitalValues({ email: '', password: '' }),
+    validate: validationSchema,
+    onSubmit: () => { }
+  });
+
+  return (
+    <div>
+      Login Page
+    </div>
+  );
+}
+
+export default Login;
